8f81b0de21eef928b9b2944e32ae92f7785de3e0
David Erroll <daviderroll@Macintosh.local>
2018-05-21 13:03:07 -0400

    ..

--- Gini_100_equals.py
+++ Gini_100_equals.py
@@ -1,8 +1,19 @@
 # This is a Monte Carlo simulation to estimate the expected Gini coefficient for 100 equal actors' accumulated points over a set number of plays.
 
-# Create list of players
+# Create list of 100 players
 
 player = 'player_'
-player_list = [player + 'i' for i in range(100)]
+player_list = [[player + 'i'] for i in range(100)]
 
-print(player_list)
+# Create randomizable list of possible scores
+
+scores = [[i] for i in range(1, 100)]
+
+# Create randomized score result
+
+import random
+random.seed(1)
+
+random_scores = shuffle(scores)
+
+results_list = player_list.append(random_scores)
