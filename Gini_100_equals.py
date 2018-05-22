be382c28d7904ee2afc8f10b31ffbb576069020c
David Erroll <daviderroll@Macintosh.local>
2018-05-22 14:37:49 -0400

    ..

--- Gini_100_equals.py
+++ Gini_100_equals.py
@@ -5,9 +5,11 @@ import random as rd
 
 rd.seed(1)
 
+total_samples = 100
+
 # Create a key of 100 players
-players = [[i for i in range(1000000, 1000100)]]
-np_player_key = np.array(players)
+#players = [[i for i in range(1000000, 1000100)]]
+#np_player_key = np.array(players)
 
 # Create list of 100 possible scores
 scores = [i for i in range(100)]
@@ -17,8 +19,8 @@ np_temp = np.array(temp_scores)
 # Create results array
 np_results = np.append(np_player_key, np_temp, axis = 0)
 
-# Shuffle scores, create np score array, and append
-for i in range(130):
+# Shuffle scores, create score array, and append
+for i in range(total_samples + 30):
     rd.shuffle(scores)
     x = [scores]
     np_scores = np.array(x)
@@ -28,7 +30,7 @@ for i in range(130):
 np_results = np.delete(np_results, 1, 0)
 
 # Create array of rolling totals
-for j in range(100):
+for j in range(total_samples):
     rolling_sum = [sum(np_results[j:j+30], 0)]
     if j == 0:
         np_rolling_sums_30 = rolling_sum
