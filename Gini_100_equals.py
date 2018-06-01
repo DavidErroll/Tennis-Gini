3d89d3877085fc27f25ccf1c70873e0857e048dc
David Erroll <daviderroll@Macintosh.local>
2018-06-01 15:30:44 -0400

    ..

--- Gini_100_equals.py
+++ Gini_100_equals.py
@@ -1,6 +1,8 @@
 # The basic premise is estimating an expected Gini coefficient for the results of repeated game play among participants that are equally skilled – i.e., equally likely to finish any given game play event in any of 100 places.
 
-# This Monte Carlo simulation code creates a 30-play total for 100 players that have been randomly assigned scores of 0 to 99.  This process is then iterated 10,000 times.
+# This Monte Carlo simulation code creates a 30-play total for 100 players that have been randomly assigned scores of 0 to 99.  These results are then summed for each player and sorted by value, from which a Gini coefficient can be determined for that 30-play instance.
+
+# This process is then iterated 10,000 times.
 
 # For each of the Gini percentiles, we calculate the arithmetic mean (μ) and standard deviation (σ) of the 10,000 samples. We then plot Lorenze curves for: (1) each 30-play result (gray); (2) the curve of mean values (solid black); and (3) the upper and lower limits of a confidence band connecting the points at μ ± 2σ (broken black).
 
@@ -32,3 +34,7 @@ np_sums = np.array([sums_list])
 
 # Sum all scores in np_results
 temp_sums = np.sum(a = np_results, axis = 0)
+
+print(np_results)
+print(np_sums)
+print(temp_sums)
